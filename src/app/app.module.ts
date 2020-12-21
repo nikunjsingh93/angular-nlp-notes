e8f32a886748a69cc55849f554b49cc5d27130bb
nikunjsingh93 <nikunj.singh@live.com>
2020-12-20 19:56:17 -0600

    added dash component and installed tljs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashComponent } from './dash/dash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashComponent
   ],
   imports: [
     BrowserModule
